5cb6e8d1143d6d0814014badab55c79b74a72609
Mike Reyes <mikereyes@Mikes-MacBook-Pro.local>
2023-05-30 23:08:45 -0500

    fixing identing 2

--- DemoId/src/main/java/demo/resources/ExtentReporterNG.java
+++ DemoId/src/main/java/demo/resources/ExtentReporterNG.java
@@ -5,15 +5,15 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentReporterNG {
 	
-	public static ExtentReports getReportObject () {
-		String path=System.getProperty("user.dir")+"/reports/log.html";
-		ExtentSparkReporter reporter=new ExtentSparkReporter(path);
-		reporter.config().setReportName("Web Automation Result");
-		reporter.config().setDocumentTitle("Test results");
+    public static ExtentReports getReportObject () {
+        String path=System.getProperty("user.dir")+"/reports/log.html";
+        ExtentSparkReporter reporter=new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Result");
+        reporter.config().setDocumentTitle("Test results");
 	
-		ExtentReports extent = new ExtentReports();
-		extent.attachReporter(reporter);
+        ExtentReports extent = new ExtentReports();
+        extent.attachReporter(reporter);
 		
-		return extent;
+        return extent;
 	}
 }
--- DemoId/src/main/java/demo/resources/Listeners.java
+++ DemoId/src/main/java/demo/resources/Listeners.java
@@ -9,59 +9,59 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 
 public class Listeners implements ITestListener{
-	ExtentTest test;
-	ExtentReports extent=ExtentReporterNG.getReportObject();
-	
-	public void onTestStart(ITestResult result) {
-		test=extent.createTest(result.getMethod().getMethodName());
-	}
-
-	public void onTestSuccess(ITestResult result) {
-		test.log(Status.PASS, "Test Passed");
-	}
-	
-	public void onTestFailure(ITestResult result) {
-		WebDriver driver=null;
-
-		try {
-			driver =(WebDriver)result.getTestClass().getRealClass().getDeclaredField("driver")
-					.get(result.getInstance());
-		} catch (Exception e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-	
-		test.log(Status.FAIL, "Test Failed");
-		String filepath=null;
-		
-		try {
-			 filepath = Keywords.takeScreenShot(driver, result.getMethod().getMethodName());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		test.addScreenCaptureFromPath(filepath,result.getMethod().getMethodName());
-		test.fail(result.getThrowable());
-	}
-
-	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
-	}
-
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-	}
-
-	public void onTestFailedWithTimeout(ITestResult result) {
-		// TODO Auto-generated method stub
-	}
-
-	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
-	}
-
-	public void onFinish(ITestContext context) {
-		extent.flush();
-	}
+    ExtentTest test;
+    ExtentReports extent=ExtentReporterNG.getReportObject();
+    
+    public void onTestStart(ITestResult result) {
+        test=extent.createTest(result.getMethod().getMethodName());
+    }
+    
+    public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS, "Test Passed");
+    }
+    
+    public void onTestFailure(ITestResult result) {
+    	WebDriver driver=null;
+    
+    	try {
+    		driver =(WebDriver)result.getTestClass().getRealClass().getDeclaredField("driver")
+    				.get(result.getInstance());
+    	} catch (Exception e1) {
+    		// TODO Auto-generated catch block
+    		e1.printStackTrace();
+    	}
+    
+    	test.log(Status.FAIL, "Test Failed");
+    	String filepath=null;
+    	
+    	try {
+    		 filepath = Keywords.takeScreenShot(driver, result.getMethod().getMethodName());
+    	} catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+    	
+    	test.addScreenCaptureFromPath(filepath,result.getMethod().getMethodName());
+    	test.fail(result.getThrowable());
+    }
+    
+    public void onTestSkipped(ITestResult result) {
+    	// TODO Auto-generated method stub
+    }
+    
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    	// TODO Auto-generated method stub
+    }
+    
+    public void onTestFailedWithTimeout(ITestResult result) {
+    	// TODO Auto-generated method stub
+    }
+    
+    public void onStart(ITestContext context) {
+    	// TODO Auto-generated method stub
+    }
+    
+    public void onFinish(ITestContext context) {
+    	extent.flush();
+    }
 }
